fdcb07195c13d924ea810acf699af20127e7f593
Aleah Manzi <aleah.manzi@gmail.com>
2017-01-12 15:29:23 -0500

    test code added

---
+++ test/test-recipes.js
@@ -0,0 +1,93 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('Recipes', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+// GET TEST
+
+  it('should list recipes on GET', function() {
+  	return chai.request(server)
+  	.get('/recipes')
+  	.then(function(res) {
+  		res.should.have.status(200);
+  		res.should.be.json;
+  		res.body.should.be.a('array');
+  		res.body.length.should.be.above(0);
+  		res.body.forEach(function(item) {
+  			item.should.be.a('object');
+  			item.should.have.all.keys('id', 'name', 'ingredients');
+  		});
+  	});
+  });
+
+// POST TEST
+
+  it('should add a recipe on POST', function() {
+  	const newRecipe = { name: 'guac', ingredients: ['avaacado', 'tomato']};
+  	return chai.request(app)
+  	 .post('/recipes')
+  	 .send(newRecipe)
+  	 .then(function(res) {
+  	 	res.should.have.status(201);
+  	 	res.should.be.json;
+  	 	res.body.should.be.a('object');
+  	 	res.body.should.include.keys('id', 'name', 'ingredients');
+  	 	res.body.name.should.equal(newRecipe.name);
+  	 	res.body.ingredients.should.be.a('array');
+  	 	res.body.ingredients.should.include.members(newRecipe.ingredients);
+  	 });
+  });
+
+  // PUT TEST
+  it('should update recipes on PUT', function() {
+    const updateData = {
+      name: 'foo',
+      ingredients: ['bizz', 'bang']
+    };
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/recipes/${updateData.id}`)
+          .send(updateData)
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.name.should.equal(updateData.name);
+        res.body.id.should.equal(updateData.id);
+        res.body.ingredients.should.include.members(updateData.ingredients);
+      });
+  });
+
+// DELETE TEST
+
+  it('should delete recipes on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`)
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+});
